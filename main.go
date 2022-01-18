d3fa90cb70d56bb866e44da6d9717614eb7f8756
Chris Hager <chris@linuxuser.at>
2022-01-18 18:17:57 +0100

    httplogger

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package goutils
 
 import "os"
 
+func Check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func GetEnv(key string, defaultValue string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
